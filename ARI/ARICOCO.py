586f4a6611818ca4fae8c323629143b68a02daa4
F-jie <fjmomy@gmail.com>
2022-01-26 23:17:39 +0800

    FEAT：linemod转coco OK！！

--- ARI/ARICOCO.py
+++ ARI/ARICOCO.py
@@ -1,14 +1,17 @@
-
+import os
+import cv2
 from ARI.ARIAnnotation import ARIDataset
+from TAU.TAUUtils import draw2DBBOX
 
 
 class ARICOCO(object):
 
     def __init__(self, dataset: ARIDataset) -> None:
         super().__init__()
-        self.datasets = dataset
+        self.dataset = dataset
+        self.jsonData = self.json()
 
-    def info():
+    def info(self):
         return {
             "description":"This is stable 1.0 version of the 2014 MS COCO dataset.",
             "url":"http:\/\/mscoco.org",
@@ -18,21 +21,59 @@ class ARICOCO(object):
             "date_created":"2015-01-27 09:11:52.357475"
         }
 
-    def licenses():
+    def licenses(self):
         return [{
             "url":"http:\/\/creativecommons.org\/licenses\/by-nc-sa\/2.0\/",
             "id":1,
             "name":"Attribution-NonCommercial-ShareAlike License"
         }]
 
-    def images():
-        pass
+    def images(self):
+        images = []
+        for id, image in enumerate(self.dataset.images):
+            images.append({
+                "license": 0,
+                "file_name": "{:04d}.png".format(id),
+                "coco_url": "",
+                "height": 480,
+                "width": 640,
+                "date_captured": "",
+                "flickr_url": "",
+                "id": id
+            })
+        return images
 
-    def annotations():
-        pass
+    def annotations(self):
+        annotations = []
+        numberOfAnno = 0
+        for id, image in enumerate(self.dataset.images):
+            for anno in image.annotations:
+                annotations.append({
+                    "id": numberOfAnno,
+                    "image_id": id,
+                    "category_id": self.queryIDWithClsID(anno.cls),
+                    "segmentation": [0, 0],
+                    "area": anno.bbox[2] * anno.bbox[3],
+                    "bbox": anno.bbox,
+                    "iscrowd": 1,
+                })
+                numberOfAnno = numberOfAnno + 1
+        return annotations
 
-    def categories():
-        pass
+    def categories(self):
+        categories = []
+        for id, clsID in enumerate(self.dataset.clses):
+            categories.append({
+                "supercategory": "none",
+                "id": id,
+                "name": clsID
+            })
+        return categories
+
+    def queryIDWithClsID(self, clsID):
+        for item in self.categories():
+            if item['name'] == clsID:
+                return item['id']
 
     def json(self):
         return {
@@ -42,3 +83,15 @@ class ARICOCO(object):
             "annotations": self.annotations(),
             "categories": self.categories()
         }
+
+    def visbbox(self, sourceDir, annoID):
+        numberOfAnno = len(self.jsonData['annotations'])
+        assert annoID < numberOfAnno, "annoID shold less than {}".format(numberOfAnno)
+        anno = self.jsonData['annotations'][annoID]
+        imagePath = os.path.join(sourceDir, "{:04d}.png".format(anno['image_id']))
+        image = cv2.imread(imagePath)
+        image = draw2DBBOX(image.copy(), anno['bbox'])
+        cv2.imshow("ImageWithBBOX2D", image)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+        
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from cv2 import line
+import os
 from ARI.Linemod import Linemod
+from ARI.ARICOCO import ARICOCO
 
 if __name__ == "__main__":
     sourceDir = "E:\code\PoseFuse\data\Linemod_preprocessed"
     linemod = Linemod(sourceDir, "linemod")
-    linemod.images[1].visBBOX3D()
+    coco = ARICOCO(linemod)
+
+    coco.visbbox(os.path.join(sourceDir, "data\\02\\rgb"), 20000)
