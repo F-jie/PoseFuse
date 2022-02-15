4160e050770c58e7413194c8fe00a57468f21d13
F-jie <fjmomy@gmail.com>
2022-02-16 01:35:21 +0800

    FEAT: Backbone

---
+++ TAU/model/position_encoding.py
@@ -0,0 +1,16 @@
+from TAU.utils.TAUUtils import NestedTensor
+from torch import nn
+
+
+class PositionEmbedingSine(nn.Module):
+
+    pass
+
+
+class PositionEmbedingLearned(nn.Module):
+
+    pass
+
+
+def build_position_encoding(args):
+    pass 
